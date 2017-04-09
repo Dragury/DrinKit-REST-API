96bb4b906600d41c7ee24bdbc26748da8bfce14d
Elliot Hargreaves <Elliot.a.h@outlook.com>
2017-04-09 20:24:37 +0100

    Implement Drink Type and sanitization

--- backendAPI.py
+++ backendAPI.py
@@ -1,69 +1,84 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_mysqldb import MySQL
-from getpass import getpass
 from datetime import datetime, timedelta
-import string, random
+import string, random, sys
 
 app = Flask(__name__)
 api = Api(app)
 app.config['MYSQL_DB'] = "drinKit"
 app.config['MYSQL_HOST'] = "127.0.0.1"
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = None
+app.config['MYSQL_USER'] = sys.argv[1]
+app.config['MYSQL_PASSWORD'] = sys.argv[2]
+mysql = MySQL(app)
 
 
 class Authenticate(Resource):
-    def get(self, auth):
+    def get(self, auth=None):
+        if auth is None:
+            return jsonify(None)
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Auth WHERE CODE='{}'".format(auth))
+        cursor.execute("SELECT * FROM Auth WHERE CODE=%s", [auth])
         res = cursor.fetchone()
         if res is not None:
             access_time = datetime.now()
-            query = "UPDATE Auth SET Expiry='{}' WHERE CODE='{}'".format(
-                "{:%Y-%m-%d %H:%M:%S}".format(access_time + timedelta(hours=1)),
-                auth
-            )
             if res['Expiry'] < access_time:
-                query = "DELETE FROM Auth WHERE CODE='{}'".format(auth)
+                query = "DELETE FROM Auth WHERE CODE=%s"
+                cursor.execute(query, [auth])
                 auth = None
-            cursor.execute(query)
+            else:
+                query = "UPDATE Auth SET Expiry=%s WHERE CODE=%s"
+                cursor.execute(query, [
+                    "{:%Y-%m-%d %H:%M:%S}".format(access_time + timedelta(hours=1)),
+                    auth
+                ])
             mysql.connection.commit()
             return jsonify(auth)
         return jsonify(None)
 
-    def post(self, auth):
+    def post(self, auth=None):
         if request.form['USER'] == app.config['MYSQL_USER'] and request.form['PASS'] == app.config['MYSQL_PASSWORD']:
             cursor = mysql.connection.cursor()
             access_time = datetime.now()
             print('{:%Y%M%S%d%H%m}'.format(access_time))
             key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(50))
-            cursor.execute("INSERT INTO Auth(CODE,Expiry) VALUES ('{}','{}')".format(
+            cursor.execute("INSERT INTO Auth(CODE,Expiry) VALUES (%s,%s)", [
                 key,
                 "{:%Y-%m-%d %H:%M:%S}".format(access_time + timedelta(hours=1))
-            ))
+            ])
             mysql.connection.commit()
             return jsonify(key)
         return None, 403
 
 
-class Drinks(Resource):
+api.add_resource(Authenticate, *["/auth", "/auth/<string:auth>"])
+
+
+class Drink(Resource):
+    def get(self, id=None):
+        cursor = mysql.connection.cursor()
+        if id is None:
+            cursor.execute("SELECT * FROM Drinks")
+            res = cursor.fetchall()
+            return jsonify(res)
+        else:
+            id = int(id)
+            cursor.execute("SELECT * FROM Drinks WHERE ID=%s", [id])
+            return jsonify(cursor.fetchone())
+
+
+api.add_resource(Drink, *["/drink", "/drink/<int:id>"])
+
+
+class DrinkTypes(Resource):
     def get(self):
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM Drinks")
-        res = cursor.fetchall()
-        for i in range(len(res)):
-            cursor.execute("SELECT * FROM Ingredients WHERE DrinkID={}".format(res[i]['ID']))
-            res[i]['Ingredients'] = cursor.fetchall()
-        return jsonify(res)
+        cursor.execute("SELECT * FROM Drink_Types")
+        return jsonify(cursor.fetchall())
 
 
-api.add_resource(Drinks, "/drinks")
-api.add_resource(Authenticate, "/auth/<string:auth>")
+api.add_resource(DrinkTypes, "/drink/types")
 
 if __name__ == "__main__":
-    app.config['MYSQL_USER'] = input("MySQL Username: ")
-    app.config['MYSQL_PASSWORD'] = getpass("MySQL Password: ")
-    mysql = MySQL(app)
     app.run(port=1997, debug=True)
